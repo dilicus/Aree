fbfa3cac0684afff5d3817bdcc76c36ccd0938e4
Miriam Olmi <miriam.olmi@gssi.it>
2017-09-01 11:53:54 +0200

    riempito il main: sceglie la forma

--- Aree.cpp
+++ Aree.cpp
@@ -1,8 +1,20 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int main()
-{
+int main(){
 
-return 0;
+  double area=0.;
+  
+  int forma;
+  printf("Scegli forma\n");
+  printf("[1:triangolo, 2:rettangolo, 3:trapezio, 4:cerchio]\n");
+  scanf("%i",&forma);
+
+  if(forma==1)area=AreaTri();
+  if(forma==2)area=AreaRett();
+  if(forma==3)area=AreaTrap();
+  if(forma==4)area=AreaCirc();
+  else area=0.;
+
+  return area;
 }
